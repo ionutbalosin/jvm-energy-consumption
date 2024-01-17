53c1e64f5cffcfcaeb0b6fcd535c7dfeea9769f4
Ionut Balosin <ionutbalosin@yahoo.com>
2024-01-17 12:51:20 +0100

    Update license

--- rapl-reports/src/main/java/com/ionutbalosin/jvm/energy/consumption/EnergyReportCalculator.java
+++ rapl-reports/src/main/java/com/ionutbalosin/jvm/energy/consumption/EnergyReportCalculator.java
@@ -3,8 +3,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2023 Ionut Balosin
- * Copyright (c) 2023 Ko Turk
+ * Copyright (c) 2023-2024 Ionut Balosin, Ko Turk
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
