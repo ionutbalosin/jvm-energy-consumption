aa52469e0840f7118b428eaf18509d33af6c63cf
Ionut Balosin <ionutbalosin@yahoo.com>
2024-02-08 07:48:09 +0100

    Update wrk test plan

--- off-the-shelf-applications/spring-petclinic/test-plan.lua
+++ off-the-shelf-applications/spring-petclinic/test-plan.lua
@@ -31,6 +31,8 @@ local requests = {}
 local petTypes = { "bird", "cat", "dog", "hamster", "lizard", "snake" }
 local dates = { "2001-01-01", "2002-02-02", "2003-03-03", "2004-04-04", "2005-05-05", "2006-06-06", "2007-07-07", "2008-08-08", "2009-09-09", "2010-10-10", "2010-11-11", "2010-12-12" }
 local headers = {["Content-Type"] = "application/x-www-form-urlencoded"}
+local ownerUrlPattern = '/owners/(%d+)$'
+local addPetVisitUrlPattern = '<td><a href="([^"]+/pets/%d+/visits/new)">Add Visit</a></td>'
 
 function setup(thread)
     thread:set("owner_url", nil)
@@ -40,13 +42,13 @@ function setup(thread)
     table.insert(threads, thread)
 end
 
-function random_number()
+function getRandomNumber()
     return math.random(100000000, 999999999)
 end
 
-function request_create_owner()
+function requestCreateOwner()
     local path = "/owners/new"
-    local randomNumber = random_number()
+    local randomNumber = getRandomNumber()
     local randomDate = dates[math.random(1, #dates)]
     local randomPetType = petTypes[math.random(1, #petTypes)]
     local owner = "firstName=Owner" ..
@@ -58,7 +60,7 @@ function request_create_owner()
     return wrk.format("POST", path, headers, owner)
 end
 
-function request_get_owner()
+function requestGetOwner()
     local path = wrk.thread:get("owner_url")
     if not path then
         return nil -- no further requests to execute
@@ -67,14 +69,14 @@ function request_get_owner()
    return wrk.format("GET", path, headers, nil)
 end
 
-function request_create_pet()
+function requestCreatePet()
     local owner_url = wrk.thread:get("owner_url")
     if not owner_url then
         return nil -- no further requests to execute
     end
 
     local path = owner_url .. "/pets/new"
-    local randomNumber = random_number()
+    local randomNumber = getRandomNumber()
     local randomDate = dates[math.random(1, #dates)]
     local randomPetType = petTypes[math.random(1, #petTypes)]
     local pet = "name=Pet-" .. randomNumber ..
@@ -84,14 +86,14 @@ function request_create_pet()
    return wrk.format("POST", path, headers, pet)
 end
 
-function request_create_pet_visit()
+function requestCreatePetVisit()
     local pet_visit_url = wrk.thread:get("pet_visit_url")
     if not pet_visit_url then
         return nil -- no further requests to execute
     end
 
     local path = "/owners/" .. pet_visit_url
-    local randomNumber = random_number()
+    local randomNumber = getRandomNumber()
     local randomDate = dates[math.random(1, #dates)]
     local pet_visit = "description=Visit-" .. randomNumber ..
         "&date=" .. randomDate
@@ -100,10 +102,10 @@ function request_create_pet_visit()
 end
 
 function init(args)
-    requests[1] = request_create_owner
-    requests[2] = request_create_pet
-    requests[3] = request_get_owner
-    requests[4] = request_create_pet_visit
+    requests[1] = requestCreateOwner
+    requests[2] = requestCreatePet
+    requests[3] = requestGetOwner
+    requests[4] = requestCreatePetVisit
 
     return requests
 end
@@ -124,18 +126,17 @@ end
 function response(status, headers, body)
     if status >= 200 and status < 300 then
         -- attempt to read the add pet visits URL from the HTML response; not available otherwise (due to server limitation)
-        local add_pet_visit_url_pattern = '<td><a href="([^"]+/pets/%d+/visits/new)">Add Visit</a></td>'
-        local is_add_pet_visit_url = body:match(add_pet_visit_url_pattern)
-        if is_add_pet_visit_url then
-            wrk.thread:set("pet_visit_url", is_add_pet_visit_url)
+        local isAddPetVisitUrl = body:match(addPetVisitUrlPattern)
+        if isAddPetVisitUrl then
+            wrk.thread:set("pet_visit_url", isAddPetVisitUrl)
         end
     end
 
     -- in general, the redirection is back to the '/owners/{ownerId}' home page.
     if status >= 300 and status < 400 then
         local redirect_url = headers["Location"]
-        local is_owners_url = string.match(redirect_url, "/owners/(%d+)$")
-        if is_owners_url then
+        local isOwnersUrl = string.match(redirect_url, ownerUrlPattern)
+        if isOwnersUrl then
             wrk.thread:set("owner_url", redirect_url)
         end
     end
